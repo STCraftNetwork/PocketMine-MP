65b1d48bb334c9e5f4b24ac065e144a401998579
KanadeBlue <124839201+KanadeBlue@users.noreply.github.com>
2024-09-29 14:02:27 +0100

    added back the pocketmine PG

--- src/world/generator/normal/Normal.php
+++ src/world/generator/normal/Normal.php
@@ -1,5 +1,24 @@
 <?php
 
+/*
+ *
+ *  ____            _        _   __  __ _                  __  __ ____
+ * |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___      |  \/  |  _ \
+ * | |_) / _ \ / __| |/ / _ \ __| |\/| | | '_ \ / _ \_____| |\/| | |_) |
+ * |  __/ (_) | (__|   <  __/ |_| |  | | | | | |  __/_____| |  | |  __/
+ * |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|     |_|  |_|_|
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * @author PocketMine Team
+ * @link http://www.pocketmine.net/
+ *
+ *
+ */
+
 declare(strict_types=1);
 
 namespace pocketmine\world\generator\normal;
@@ -27,7 +46,9 @@ class Normal extends Generator{
 	private const WORLD_MAX_HEIGHT = 320;
 	private const DEEPSLATE_START = 0;
 	private int $waterHeight = 62;
+	/** @var Populator[] */
 	private array $populators = [];
+	/** @var Populator[] */
 	private array $generationPopulators = [];
 	private Simplex $noiseBase;
 	private BiomeSelector $selector;
